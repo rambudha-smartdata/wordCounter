d19b4a2aac2713e0ff45df649d0c87dc46cde1af
oorochimaru <ryambudha@gmail.com>
2019-05-09 04:38:54 +0530

    mongodb URI from env

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 # clone the repo
 - wordCounterFrontend consists the Angular 7 fontend for the application
 - wordCounterBackend consists the ExpressJs backend with mongodb as database.
+
+
+# variable to be set
+- NODE_ENV
+- MONGODB_URI
+- PORT
--- wordCounterBackend/src/app.js
+++ wordCounterBackend/src/app.js
@@ -5,12 +5,26 @@ import cors from 'cors';
 import logger from 'morgan';
 import mongooseConnection from './config/db';
 import indexRouter from './routes/index';
+import errorhandler from 'errorhandler';
 
 const app = express();
+
+var isProduction = process.env.NODE_ENV === 'production';
+
 
 // Cross platform origin resource sharing
 app.use(cors());
 
+if (!isProduction) {
+    app.use(errorhandler());
+}
+
+if (isProduction) {
+    mongoose.connect(process.env.MONGODB_URI);
+} else {
+    mongoose.connect('mongodb://localhost/wordcounter');
+    mongoose.set('debug', true);
+}
 // Database connection
 mongooseConnection();
 
@@ -22,5 +36,38 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../public')));
 
 app.use('/api', indexRouter);
+// will print stacktrace
+if (!isProduction) {
+    app.use(function (err, req, res, next) {
+        console.log(err.stack);
+
+        res.status(err.status || 500);
+
+        res.json({
+            'errors': {
+                message: err.message,
+                error: err
+            }
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.json({
+        'errors': {
+            message: err.message,
+            error: {}
+        }
+    });
+});
+
+// finally, let's start our server...
+var server = app.listen(process.env.PORT || 3000, function () {
+    console.log('Listening on port ' + server.address().port);
+});
+
 
 export default app;
